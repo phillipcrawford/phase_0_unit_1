2d9ce3da686affb6111a36a86916735f6f95b2ec
phillipcrawford <phillip.crawford3@gmail.com>
2014-04-17 18:02:43 -0700

    pseudocode

---
+++ week_3/5_group_project/gc1_pseudocode.js
@@ -0,0 +1,27 @@
+// Make empty array
+// ask user for input
+// collect input from user
+// while input is a number
+//     push input to array
+
+// define function sum that takes array as parameter
+// make new variable sum equal to zero
+// make for loop that goes from 0 up to length of array
+//     add array value to sum
+// return sum
+
+// define function median that takes array as parameter
+// make new variable temp = array length / 2
+// if length of array % 2 equals 1
+//     return array[floor(temp)]
+// else 
+//     return (array[temp] + array [temp-1])/2
+
+// define function mean that takes array as parameter
+// make new variable mean
+// set mean equal to array.sum divided by length of array
+// return mean
+
+// print statement sum is array.sum
+// print statement median is array.median
+// print statement mean is array.mean
